95e4df28172064cb0a17b55a159b79c3ddda3b94
mikami <mikami@tecpresso.co.jp>
2019-08-17 20:13:45 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from selenium import webdriver
+
+
+FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "screen.png")
+
+driver = webdriver.Chrome()
+driver.get("https://webglsamples.org/aquarium/aquarium.html")
+
+driver.save_screenshot(FILENAME)
+
+driver.quit()
