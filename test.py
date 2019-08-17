86392c215e201de3cb73e4c4348be0f146aed8c4
mikami <mikami@tecpresso.co.jp>
2019-08-17 20:42:06 +0900

    WIP

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "screen.png")
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('chromedriver.exe')
 driver.get("https://webglsamples.org/aquarium/aquarium.html")
 
 driver.save_screenshot(FILENAME)
